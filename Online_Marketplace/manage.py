ecb1b79172b021604de7d24f274e79763d95b3c8
Mamba1099 <mambakaranja240@gmail.com>
2023-11-14 14:59:59 +0300

    refactor: changed folder name to avoid colliding

--- Online_Marketplace/manage.py
+++ Online_Marketplace/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Online_Marketplace/Online_Marketplace.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Online_Marketplace/My_app.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

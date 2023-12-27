b32ca63ed1c7eea96da8f6aae9e862245ddda4e2
Mamba1099 <mambakaranja240@gmail.com>
2023-12-27 18:20:27 +0300

    register new app in setings

--- My_app/urls.py
+++ My_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("item/", include('item.urls')),
     path("", include("core.urls")),
+    path("dashboard/", include("dashboard.urls")),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

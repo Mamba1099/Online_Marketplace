068c0457239e84090161ab143cd5de097a71defb
Mamba1099 <mambakaranja240@gmail.com>
2023-12-27 18:27:19 +0300

    initial commit

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views 
+
 
 from . import views
 from .forms import LoginForm
@@ -10,6 +11,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("contact/", views.contact, name="contact"),
     path("signup/", views.signup, name="signup"),
-    path("login/", auth_views.LoginView.as_view(template_name='login.html',authentication_form=LoginForm ), name="login")
-    
+    path("login/", auth_views.LoginView.as_view(template_name='login.html',authentication_form=LoginForm ), name="login"),
+    path("logout/", views.logout_then_login, name="logout")
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.views import LogoutView
+from django.conf import settings
+from django.http import HttpResponseRedirect, QueryDict
+from urllib.parse import urlparse, urlunparse
 from item.models import Category, Item
 
 from .forms import SignupForm
+
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -30,3 +34,22 @@ def signup(request):
     return render(request, 'signup.html', {
         'form': form
     })
+
+def logout_then_login(request, login_url='login'):
+
+    login_url = (settings.LOGIN_URL)
+    return LogoutView.as_view(next_page=login_url)(request)
+
+def redirect_to_login(next, redirect_field_name='logout'):
+    """
+    Redirect the user to the login page, passing the given 'next' page.
+    """
+    resolved_url = (settings.LOGIN_URL)
+
+    login_url_parts = list(urlparse(resolved_url))
+    if redirect_field_name:
+        querystring = QueryDict(login_url_parts[4], mutable=True)
+        querystring[redirect_field_name] = next
+        login_url_parts[4] = querystring.urlencode(safe="/")
+
+    return HttpResponseRedirect(urlunparse(login_url_parts))

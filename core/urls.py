f543d6c42a3bf7b2d6c095ba6fa84cbe33d52139
Mamba1099 <mambakaranja240@gmail.com>
2023-12-27 11:02:34 +0300

    import module

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, contact
+
+from . import views
 
 app_name = "core"
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("contact/", contact, name="contact"),
+    path("", views.index, name="index"),
+    path("contact/", views.contact, name="contact"),
 ]

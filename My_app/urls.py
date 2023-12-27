8f0b08a247659b601ed6a610d8af0fbce4434bda
Mamba1099 <mambakaranja240@gmail.com>
2023-12-27 18:53:41 +0300

    add patterns for dashboard app

--- My_app/urls.py
+++ My_app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("item/", include('item.urls')),
     path("", include("core.urls")),
-    path("dashboard/", include("dashboard.urls")),
+    path("", include("dashboard.urls")),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

599424f96e4cc78d4d2a1e08b01f5d345f258333
Getonto3 <getontomoses18@gmail.com>
2024-01-04 18:44:07 +0300

    refactor

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,8 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('', include('core.urls')),
-    path('items/', include('item.urls')),
+    path('', include('item.urls')),
     # path('dashboard/', include('dashboard.urls')),
     # path('inbox/', include('conversation.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

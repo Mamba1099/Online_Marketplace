ddf9c1406f432fbfc47acf6e2f4979c282c1e91c
Mamba1099 <mambakaranja240@gmail.com>
2024-01-04 22:27:17 +0300

    register new app

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # third party apps
     "rest_framework",
+    "dashboard",
     "corsheaders",
     # project apps
     "core",
--- item/views.py
+++ item/views.py
@@ -97,4 +97,4 @@ def delete(request, pk):
     item = get_object_or_404(Item, pk=pk, created_by=request.user)
     item.delete()
 
-    return redirect("index.html")
+    return redirect("dashboard:index.html")

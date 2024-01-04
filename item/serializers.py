6405cff833b206bf716c3ca24c91f2a9032664dc
Getonto3 <getontomoses18@gmail.com>
2024-01-04 15:28:20 +0300

    feat:create generic views using serializers

---
+++ item/serializers.py
@@ -0,0 +1,22 @@
+from item.models import Category,Item
+from rest_framework import serializers
+
+class CategorySerialiZer(serializers.ModelSerializer):
+     
+    class Meta:
+        model = Category
+        fields = ["__all__"]
+        
+class ItemSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Item
+        fields = [
+            "category",
+            "name",
+            "description",
+            "price",
+            "is_sold",
+            "created_by",
+            "created_at",
+            ]

3ffbc0356dc342cc935276e5ca5998741c97563b
Getonto3 <getontomoses18@gmail.com>
2024-01-04 15:29:12 +0300

    register urls for generic class based views

--- item/urls.py
+++ item/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
 app_name = 'item'
 
 urlpatterns = [
-    path('', views.items, name='items'),
-    path('new/', views.new, name='new'),
-    path('<int:pk>/', views.detail, name='detail'),
-    path('<int:pk>/delete/', views.delete, name='delete'),
-    path('<int:pk>/edit/', views.edit, name='edit'),
+    path('category/', views.CategoryList.as_view()),
+    path('category/<int:pk>/', views.CategoryDetail.as_view()),
+    path('item/', views.ItemList.as_view()),
+    path('item/<int:pk>/', views.ItemDetail.as_view()),
+    
 ]
 

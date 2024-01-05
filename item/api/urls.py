db80cc46a1cab54a42aa132bd5fae8b520881c7f
Getonto3 <getontomoses18@gmail.com>
2024-01-05 18:09:25 +0300

    refactor

--- item/api/urls.py
+++ item/api/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("category/<int:pk>/", views.CategoryDetail.as_view()),
     path("item/", views.ItemList.as_view()),
     path("item/<int:pk>/", views.ItemDetail.as_view()),
-    path("users/", views.UserList.as_view()),
-    path('users/<int:pk>/', UserCreateView.as_view()),
+    path("user/", views.UserList.as_view()),
+    path('users/', UserCreateView.as_view()),
 ]

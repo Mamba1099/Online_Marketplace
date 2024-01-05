a4c11518d6d1baa0d89b8757169dfe9f0307e13c
Mamba1099 <mambakaranja240@gmail.com>
2024-01-05 18:08:39 +0300

    fix api path

--- item/api/urls.py
+++ item/api/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("category/<int:pk>/", views.CategoryDetail.as_view()),
     path("item/", views.ItemList.as_view()),
     path("item/<int:pk>/", views.ItemDetail.as_view()),
-    path("users/", views.UserList.as_view()),
-    path('users/<int:pk>/', UserCreateView.as_view()),
+    path("registered-list/", views.UserList.as_view()),
+    path("user-register/", UserCreateView.as_view()),
 ]
